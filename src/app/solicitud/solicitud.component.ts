c8d2689c9de4218457b0fa7c2cab1fcda0f70838
pozdiacr <pozdiacr@jcyl.es>
2021-06-01 20:05:49 +0200

    Ejercicios Tema 1

--- src/app/solicitud/solicitud.component.ts
+++ src/app/solicitud/solicitud.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    setInterval(() => {
+      this.solicitud.nombre = '' + Math.random();
+      }, 2000)
+     
+  }
 
   ngOnInit(): void {
   }
+
+  solicitud = {nombre: "Cristina",
+              apellido1: "",
+              apellido2: ""};
+  
+  delete() {
+      console.log('Eliminar!')
+  }
+
+  pulsar($event: MouseEvent) {
+    console.log('Hola!')    
+  }
+
+  apellido1($event: KeyboardEvent) {
+    const element = $event.target as HTMLInputElement
+    this.solicitud.apellido1 = element.value;    
+   }
+
+  apellido2($event: KeyboardEvent) {
+    const element = $event.target as HTMLInputElement
+    this.solicitud.apellido2 = element.value;    
+   }
 
 }
